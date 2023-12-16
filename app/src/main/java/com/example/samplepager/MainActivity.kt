aaa4790ff205d331ad5e63c0e2ebffa075af2285
RYUJIKOYAMANO\ryuji <ryujikoyama95@gmail.com>
2023-12-17 02:00:53 +0900

    Add sample code

--- app/src/main/java/com/example/samplepager/MainActivity.kt
+++ app/src/main/java/com/example/samplepager/MainActivity.kt
@@ -4,15 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.samplepager.ui.theme.SamplePagerTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,12 +41,31 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun pager() {
-    HorizontalPager(
-        pageCount = 3
+    Column(
+        modifier = Modifier.verticalScroll(rememberScrollState())
     ) {
-        Text(
-            text = "$it",
-            modifier = Modifier.fillMaxHeight()
-        )
+        HorizontalPager(
+            pageCount = 3
+        ) {
+            Text(
+                text = "$it"
+            )
+        }
+        Spacer(modifier = Modifier.padding(vertical = 10.dp))
+        HorizontalPager(
+            pageCount = 3
+        ) {
+            Text(
+                text = "$it"
+            )
+        }
+        Spacer(modifier = Modifier.padding(vertical = 10.dp))
+        HorizontalPager(
+            pageCount = 3
+        ) {
+            Text(
+                text = "$it"
+            )
+        }
     }
 }
